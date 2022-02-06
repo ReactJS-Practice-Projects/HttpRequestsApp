a4c26dcf57d00d83a826e65211b0478f7dbabf4d
andrey12v <andrey12v@gmail.com>
2022-02-06 16:14:15 -0600

    Add useEffect to load fetch API when the app is loaded for the first time
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599820#overview

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,19 +8,17 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  //instead of using javascript 'then' chains and promises we can async to get the same result
-  //async - is asynchronous code  
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    try{
+    try {
       const response = await fetch('https://swapi.dev/api/films/');
-      if(!response.ok)
-      {
+      if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -30,13 +28,31 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-    }
-    catch(error)
-    {
+    } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-    
+  }, []);
+
+  //we use - useEffect to fetch API when app is loadedfor the first time
+  //the we use - useCallback in fetchMoviesHandler to prevent infinite loop
+  //the instructor provided vague explanations about it 
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -44,12 +60,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
