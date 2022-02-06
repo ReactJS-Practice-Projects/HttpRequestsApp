736a38eb3186633d0e60e9371343e59f6eb0d689
andrey12v <andrey12v@gmail.com>
2022-02-05 23:08:31 -0600

    Add first Http Get request
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599796#overview

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films/')
+      .then((response) => {
+        return response.json();
+      })
+      //The API returns the response as "results": [{ "title": "A new movie", etc..}
+      //That's why we use data.results.map
+      //in other words we transform results into the object with the set of fields 
+      //this object is assigned to movies variable after that 
+      //we assign values from the API to the new fields such as opening_crawl to openingText
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
